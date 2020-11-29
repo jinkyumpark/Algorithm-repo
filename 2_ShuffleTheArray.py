10dd0480d3dfe0821efe042f37f1e8666b3c5e8a
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2020-11-30 00:05:27 +0900

    Update 2_ShuffleTheArray.py

--- 2_ShuffleTheArray.py
+++ 2_ShuffleTheArray.py
@@ -3,8 +3,8 @@
 #Shuffle array of the form [x1, x2, ... , xn, y1, y2, ... , yn]
 #to [x1, y1, x2, y2, ... , xn, yn]
 
-#Runtime: -94.99%
-#Memory Usage: -14.58%
+#Runtime: -94.99%, 52ms
+#Memory Usage: -14.58%, 14.5MB
 
 class Solution():
     def shuffle(self, nums:list[int], n: int) -> list[int]:
