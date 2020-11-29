2229d645536e54758f02d8a2c4551e9c7811b332
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2020-11-30 00:04:41 +0900

    Create 2_ShuffleTheArray.py

---
+++ 2_ShuffleTheArray.py
@@ -0,0 +1,15 @@
+#Shuffle the Array - from Leetcode 1470
+
+#Shuffle array of the form [x1, x2, ... , xn, y1, y2, ... , yn]
+#to [x1, y1, x2, y2, ... , xn, yn]
+
+#Runtime: -94.99%
+#Memory Usage: -14.58%
+
+class Solution():
+    def shuffle(self, nums:list[int], n: int) -> list[int]:
+        out_list: list[int] = []
+        for i in range(n):
+            out_list.append(nums[i])
+            out_list.append(nums[n+i])
+        return out_list
