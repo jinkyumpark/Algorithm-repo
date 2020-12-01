dc10a37ed20a3aa4515d07439a8650622a8e5426
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2020-12-01 16:03:09 +0900

    Update 2_EvenNumberDigit.py

--- 2_EvenNumberDigit.py
+++ 2_EvenNumberDigit.py
@@ -1,13 +1,17 @@
-#Unsolved
 #Find Numbers with Even Number of Digits - from Leetcode 1295
 
 #Return number of even number of digit in an array
+
+#Runtime: +13.77%
+#Memory Usage: -39.78%
+
+#Lesson learnt: 'is' is not equal to ==
 
 class Solution:
     def findNumbers(self, nums: list[int]) -> int:
         count = 0
         for item in nums:
-            if self.isDigitEven(item) is True:
+            if self.isDigitEven(item) == True:
                 count += 1
         return count
 
@@ -15,11 +19,11 @@ class Solution:
         count = 0
         divisor = 10
         remainder = -1
-        while remainder is not num:
+        while remainder != num:
             remainder = num % divisor
             divisor *= 10
             count += 1
-        if count % 2 is 0:
+        if count % 2 == 0:
             return True
         else:
             return False
