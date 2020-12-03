b90c3999a3bf7020539d10ae369b16e00e085ca3
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2020-12-04 02:08:38 +0900

    Update and rename sorting.c to On2_sorting.c

--- Algorithm in C/On2_sorting.c
+++ Algorithm in C/On2_sorting.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-void bubbleSort(int inArr[]);
+void bubbleSort(int* inArr[]);
+void insertionSort(int* inArr[]);
+void selectionSort(int* inArr[]);
 void swap(int *num1, int *num2);
 
 int main(int argc, char* argv){
@@ -23,6 +25,21 @@ void bubbleSort(int* inArr[]){
 
 void insertionSort(int* inArr[]){
 
+}
+
+void selectionSort(int* inArr[]){
+	size_t n = sizeof(inArr)/sizeof(int);
+	for(int i = 0; i < n; i++){
+		int smallest = inArr[i];
+		int index = i;
+		for(int j = i+1; j < n; j++){
+			if(inArr[j] < smallest){
+				smallest = inArr[j];
+				index = j;
+			}
+		}
+		swap(&inArr[index], &inArr[i]);
+	}
 }
 
 void swap(int *num1, int *num2){
