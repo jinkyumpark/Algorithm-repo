e502f3ed95fe2bf909958c3b5afe63f9785b4581
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2021-01-06 04:57:42 +0900

    Create ArrayList.java

---
+++ Data Structure/Java/ArrayList.java
@@ -0,0 +1,9 @@
+// Unfixed size of array, resize 50% more size when it reaches maximum
+
+public class ArrayList{
+  private int[] array;
+
+  public ArrayList(int size){
+    this.array = new int[size];
+  }
+}
